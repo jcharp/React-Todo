8ccdf1a1a5fbafa9fde16328d1127ca647d18a7c
jcharp <61035559+jcharp@users.noreply.github.com>
2020-09-30 20:36:11 -0400

    started todoform and todolist component

---
+++ src/components/TodoForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TodoForm extends React.component {
+
+    render() {
+        return (
+            <form>
+
+            </form>
+        )
+    }
+}
+
+export default TodoForm;
